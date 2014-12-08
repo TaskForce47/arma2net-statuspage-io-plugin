11623da30afb7373e1940eeb79ccacfbd07d669d
Martin Schweizer <me@martinschweizer.de>
2014-12-08 21:27:37 +0100

    moved file structure

---
+++ StatusPageIO/StatusPageIOConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace StatusPage
+{
+    [Serializable()]
+    public class StatusPageIOConfiguration
+    {
+        public String ApiKey { get; set; }
+        public string PageId { get; set; }
+        public string MetricId { get; set; }
+        public string BaseUri { get; set; }
+        public string Identifier { get; set; }
+
+        public StatusPageIOConfiguration()
+        {
+            ApiKey = PageId = MetricId = BaseUri = Identifier = "";
+        }
+    }
+}
